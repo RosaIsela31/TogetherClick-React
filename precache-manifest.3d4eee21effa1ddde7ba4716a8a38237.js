8bd6d0921c20cb42f35bbd4025cef278f3764155
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-10-29 14:46:03 -0600

    Updates

---
+++ precache-manifest.3d4eee21effa1ddde7ba4716a8a38237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6753aafd32b44a543b6667e6553f3cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8e2ec061110acd658e2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5fd19ddbf42b428eec16",
+    "url": "/static/js/2.6513f18a.chunk.js"
+  },
+  {
+    "revision": "b8e2ec061110acd658e2",
+    "url": "/static/js/main.492f43ce.chunk.js"
+  },
+  {
+    "revision": "0041a80b1c7cc9e1dc99",
+    "url": "/static/js/runtime-main.cd8c6827.js"
+  }
+]);
